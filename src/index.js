18207a018505a0dfc90db8ea04d198e9ed7ddfcd
WhoAteDaCake <doitsimplefy@gmail.com>
2018-09-03 16:12:12 +0100

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+import Initial from "./components/Initial";
+
+const root = document.getElementById("root");
+
+render(<Initial />, root);
