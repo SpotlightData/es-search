9b2478b5a841df07480f52a4aa721bc8d955f991
WhoAteDaCake <doitsimplefy@gmail.com>
2018-09-03 16:30:20 +0100

    update devserver to add proxy

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,3 +1,16 @@
 module.exports = {
-  use: ["@neutrinojs/react-components"]
+  use: [
+    [
+      "@neutrinojs/react-components",
+      {
+        env: ["TOKEN", "URL"],
+        devServer: {
+          // Disabling options.hot will also disable devServer.hot
+          hot: true,
+          // Proxy requests that don't match a known file to the specified backend.
+          proxy: "https://127.0.0.1:9000/v1"
+        }
+      }
+    ]
+  ]
 };
