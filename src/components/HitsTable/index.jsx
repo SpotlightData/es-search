447fdf9a9c0e5387a6a98bb90d706725558fdc02
WhoAteDaCake <doitsimplefy@gmail.com>
2018-09-04 11:39:10 +0100

    hit table component

---
+++ src/components/HitsTable/index.jsx
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import {
+  string,
+  number,
+  bool,
+  arrayOf,
+  shape,
+  oneOfType,
+  func
+} from "prop-types";
+
+import { Row } from "antd";
+import {
+  SearchkitComponent,
+  SourceFilterAccessor,
+  PageSizeAccessor
+} from "searchkit";
+import { Table } from "@spotlightdata/nanowire-extensions/lib/components/antd/Table";
+import { bytesToReadable } from "@spotlightdata/nanowire-extensions/lib/helpers/table";
+import Pagination from "../Pagination";
+
+import * as R from "ramda";
+
+const inLd = name => "_source.jsonLD." + name;
+
+export default class HitsTable extends SearchkitComponent {
+  static propTypes = {
+    hitsPerPage: number,
+    sourceFilter: oneOfType([string, arrayOf(string), bool]),
+    columns: arrayOf(shape({})).isRequired,
+    formatRow: func,
+    filter: func
+  };
+  static defaultProps = {
+    hitsPerPage: 20,
+    sourceFilter: [
+      inLd("name"),
+      inLd("dateCreated"),
+      inLd("dateModifier"),
+      inLd("fileSize")
+    ],
+    formatRow: undefined,
+    filter: undefined
+  };
+
+  componentDidMount() {
+    this.searchkit
+      .getAccessorByType(PageSizeAccessor)
+      .setSize(this.props.hitsPerPage);
+    this.searchkit.addAccessor(
+      new SourceFilterAccessor(this.props.sourceFilter)
+    );
+    this.searchkit.performSearch();
+  }
+
+  format(task) {
+    const data = R.pathOr({}, ["_source", "jsonLD"], task);
+    return {
+      _id: task._id,
+      name: data.name,
+      dateCreated: data.dateCreated,
+      dateModified: data.dateModified,
+      fileSize: bytesToReadable(data.fileSize)
+    };
+  }
+
+  render() {
+    const { hitsPerPage, columns, format, filter } = this.props;
+    const formatter = format || this.format;
+    let results = this.getHits().reduce(
+      (list, item) => [formatter(item), ...list],
+      []
+    );
+    if (filter) {
+      results = results.filter(filter);
+    }
+    return (
+      <React.Fragment>
+        <Table
+          dataSource={results}
+          columns={columns}
+          pagination={false}
+          loading={this.searchkit.loading}
+        />
+        <Row type="flex" justify="end" style={{ marginTop: "1em" }}>
+          <Pagination pageSize={hitsPerPage} />
+        </Row>
+      </React.Fragment>
+    );
+  }
+}
--- src/demo/App/index.js
+++ src/demo/App/index.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { Row } from "antd";
+
+import "antd/dist/antd.css";
+
+import { HitsTable } from "../../components";
 
 export const App = () => {
-  return <div>asdasd</div>;
+  return (
+    <div style={{ padding: "2em", width: "100%", height: "100%" }}>
+      <Row>
+        <HitsTable
+          columns={[]}
+          filter={n => !Object.values(n).includes(undefined)}
+        />
+      </Row>
+    </div>
+  );
 };
