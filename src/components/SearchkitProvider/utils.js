5da8db8844c739edc5150e5362576a6b35896af6
WhoAteDaCake <doitsimplefy@gmail.com>
2018-09-04 14:36:44 +0100

    fix wrapping of sk query filters

--- src/components/SearchkitProvider/utils.js
+++ src/components/SearchkitProvider/utils.js
@@ -6,11 +6,14 @@ import {
   queryObjectToString
 } from "@spotlightdata/nanowire-extensions/lib/helpers/request";
 
-const encodeObjUrl = obj =>
-  qs.stringify(obj, { encode: true, encodeValuesOnly: true });
-const decodeObjString = str => qs.parse(str);
-const wrapQuery = str => `"${str}"`;
-const unwrapQuery = str => (str === null ? "" : R.replace(/"/gm, "", str));
+const encodeObjUrl = obj => {
+  const parsed = qs.stringify(obj, { encode: true, encodeValuesOnly: true });
+  return parsed.replace(/&/g, "+");
+};
+const decodeObjString = str => {
+  const clean = str.replace(/\+/g, "&");
+  return qs.parse(clean);
+};
 
 // Replace with nanowire extensions one when it's fixed
 function queryUrlToObject(search) {
@@ -23,44 +26,44 @@ function queryUrlToObject(search) {
     const eqIndex = pair.indexOf("=");
     const key = pair.slice(0, eqIndex);
     const value = pair.slice(eqIndex + 1, pair.length);
-    // const [key, value] = pair.split('=');
     return { ...dict, [decodeURI(key)]: decodeURI(value) };
   }, {});
 }
 
+function cleanDates(obj) {
+  if (typeof object !== "object" || !obj.date1) {
+    return obj;
+  }
+  // A fix when when miliseconds are strings rather than numbers
+  return {
+    ...obj,
+    date1: {
+      min: parseInt(obj.date1.min, 10),
+      max: parseInt(obj.date1.max, 10)
+    }
+  };
+}
+
 export const stateFromQuery = key =>
   R.pipe(
-    url => {
-      const query = queryUrlToObject(url);
-      return query[key] !== undefined ? query[key] : '""';
-    },
-    unwrapQuery,
+    url => queryUrlToObject(url)[key] || "",
     decodeObjString,
-    obj => {
-      if (typeof object !== "object" || !obj.date1) {
-        return obj;
-      }
-      // A fix when when miliseconds are strings rather than numbers
-      return {
-        ...obj,
-        date1: {
-          min: parseInt(obj.date1.min, 10),
-          max: parseInt(obj.date1.max, 10)
-        }
-      };
-    }
+    cleanDates
   );
 
 export function updateSKHistory(history, queryKey, state) {
   const query = queryUrlToObject(history.location.search);
   const prevState = stateFromQuery(queryKey)(history.location.search);
-  const newState = R.mergeDeepRight(prevState, state);
-  const objectUrl = encodeObjUrl(newState);
+  const search = R.pipe(
+    R.mergeDeepRight(prevState),
+    encodeObjUrl,
+    str => R.assoc(queryKey, str, query),
+    queryObjectToString
+  )(state);
 
-  const newQuery = R.assoc(queryKey, wrapQuery(objectUrl), query);
   const historyState = {
     ...history.location,
-    search: queryObjectToString(newQuery)
+    search
   };
   return historyState;
 }
