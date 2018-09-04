5957f66e01f56bc183e5c97974784126f699175a
WhoAteDaCake <doitsimplefy@gmail.com>
2018-09-04 11:41:37 +0100

    add demo columns

--- src/demo/App/index.js
+++ src/demo/App/index.js
@@ -1,16 +1,36 @@
 import React from "react";
+
 import { Row } from "antd";
+import { Link } from "react-router-dom";
+import { Button } from "antd";
 
 import "antd/dist/antd.css";
 
 import { HitsTable } from "../../components";
+import { defaultTimeFomat } from "@spotlightdata/nanowire-extensions/lib/helpers/table";
+
+const columns = [
+  {
+    title: "Title",
+    dataIndex: "name"
+  },
+  {
+    title: "Date",
+    dataIndex: "dateCreated",
+    render: defaultTimeFomat
+  },
+  {
+    title: "File Type",
+    dataIndex: "fileSize"
+  }
+];
 
 export const App = () => {
   return (
     <div style={{ padding: "2em", width: "100%", height: "100%" }}>
       <Row>
         <HitsTable
-          columns={[]}
+          columns={columns}
           filter={n => !Object.values(n).includes(undefined)}
         />
       </Row>
