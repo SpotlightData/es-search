86d65f0edc26fdd11b59ee7bfcabdfcc97720942
whoatedacake <doitsimplefy@gmail.com>
2018-09-12 21:02:17 +0100

    update build config

--- builder.js
+++ builder.js
@@ -7,27 +7,27 @@ const {
   bundle,
   library,
   configure,
-} = require('@atecake/builder');
+  tag,
+} = require('../builder/src');
+// } = require('@atecake/builder');
 
 const pkg = require('./package.json');
 
 start([
   configure(),
   when('build', [
-    customise(() => ({ files: { input: 'src/components/index.js', output: pkg.main } })),
-    bundle(),
-    proceed(),
-    customise(() => ({
+    bundle({ files: { input: 'src/components/index.js', output: pkg.main } }),
+    bundle({
       files: { input: 'src/components/index.js', output: pkg.module },
       build: { rollup: { format: 'esm' } },
-    })),
-    bundle(),
-    proceed(),
-    customise(() => ({
+      action: Promise.resolve(),
+    }),
+    library({
       files: { input: 'src/components', output: 'es5' },
       build: { rollup: { format: 'cjs' } },
-    })),
-    library(),
+      action: Promise.resolve(),
+    }),
   ]),
   when('start', [devServer()]),
+  when('tag', [tag()]),
 ]);
